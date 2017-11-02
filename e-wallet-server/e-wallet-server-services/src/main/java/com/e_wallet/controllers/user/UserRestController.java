ba2b5b16b4afed7099bb2be3a0c4a186f9e450d6
Redpike <rsokulski@op.pl>
2017-11-02 19:01:24 +0100

    Add new branch restapi

---
+++ e-wallet-server/e-wallet-server-services/src/main/java/com/e_wallet/controllers/user/UserRestController.java
@@ -0,0 +1,11 @@
+package com.e_wallet.controllers.user;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UserRestController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserRestController.class);
+
+
+}
