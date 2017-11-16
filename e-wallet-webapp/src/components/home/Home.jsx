e1759cc3465b9d4b966d13f9e5fa568c5fd33a1f
Rafał Myśliwiec <robbie16@vp.pl>
2017-11-16 23:49:52 +0100

    css templates integration, react materialui + react materialize

--- e-wallet-webapp/src/components/home/Home.jsx
+++ e-wallet-webapp/src/components/home/Home.jsx
@@ -1,18 +1,44 @@
 import React from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import Drawer from "material-ui/Drawer";
+import MenuItem from "material-ui/MenuItem";
+import {AppBar, IconButton} from "material-ui";
+import {NavigationClose} from "material-ui/svg-icons/index";
+import {Footer} from "react-materialize";
 
 const Home = props => (
-    <div className="Home">
-        <h1>Home</h1>
-        <p>Count: {props.count}</p>
-
-        <p>
-            <button onClick={props.increment} disabled={props.isIncrementing}>Increment</button>
-            <button onClick={props.incrementAsync} disabled={props.isIncrementing}>Increment Async</button>
-        </p>
-
-        <p>
-            <button onClick={() => props.changePage()}>Go to about page via redux</button>
-        </p>
+    <div style={{height: "100%", display: "flex", flexDirection: "column"}}>
+        <MuiThemeProvider>
+            <Drawer open={true}>
+                <MenuItem>Menu Item</MenuItem>
+                <MenuItem>Menu Item 2</MenuItem>
+            </Drawer>
+            <AppBar
+                style={{flex: "0 64px"}}
+                title="Title"
+                iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+            />
+            <div style={{flex: "1 auto"}}/>
+            <Footer copyrights="&copy 2015 Copyright Text"
+                    moreLinks={
+                        <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+                    }
+                    links={
+                        <ul>
+                            <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                            <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                            <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                            <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                        </ul>
+                    }
+                    className='example'
+                    style={{flex: "0 40px"}}
+            >
+                <h5 className="white-text">Footer Content</h5>
+                <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer
+                    content.</p>
+            </Footer>
+        </MuiThemeProvider>
     </div>
 );
 export default Home
--- e-wallet-webapp/src/index.js
+++ e-wallet-webapp/src/index.js
@@ -1,18 +1,16 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'react-router-redux'
-import store, { history } from './store'
-import App from './components/app/App.jsx'
+import React from "react";
+import {render} from "react-dom";
+import {Provider} from "react-redux";
+import {ConnectedRouter} from "react-router-redux";
+import store, {history} from "./store";
+import App from "./components/app/App.jsx";
 
 const target = document.querySelector('#root')
 
 render(
     <Provider store={store}>
         <ConnectedRouter history={history}>
-            <div>
-                <App />
-            </div>
+            <App />
         </ConnectedRouter>
     </Provider>,
     target
